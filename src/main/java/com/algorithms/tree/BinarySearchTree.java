12746bdc275a3bc134f631dc3523b7532fabe49e
linxiao <linxiao84@126.com>
2016-03-09 23:34:26 +0800

    second

--- src/main/java/com/algorithms/tree/BinarySearchTree.java
+++ src/main/java/com/algorithms/tree/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package com.algorithms.tree;
 
+import java.util.HashMap;
+import java.util.TreeMap;
+
 /**
  * Created by linxiao on 2016/3/9.
  * 二叉搜索树
@@ -24,7 +27,105 @@ public class BinarySearchTree {
             return;
         }
 
+        HashMap m;
+        TreeMap o;
+        Node current = root;
+        Node parent;
+        while (true) {
+            parent = current;
+            //比当前节点小，往左子节点寻找
+            if (data < current.getData()) {
+                current = current.getLeftChild();
+                if(current == null) {
+                    parent.leftChild = node;
+                    return;
+                }
+            }
+            //大于或等于当前节点，往右子节点寻找
+            else {
+                current = current.getRightChild();
+                if(current == null) {
+                    parent.rightChild = node;
+                    return;
+                }
+            }
+        }
+    }
 
+    /**
+     * 查询一个值
+     * @param data
+     * @return
+     */
+    public Node find(int data) {
+        Node current = root;
+        while (current.data != data) {
+            if(data < current.data) {
+                current = current.getLeftChild();
+            }else {
+                current = current.getRightChild();
+            }
+            //没有找到，返回空
+            if(current == null)
+                return null;
+        }
+        return current;
+    }
 
+    /**
+     * 先序遍历
+     * @param node
+     */
+    public void preOrder(Node node) {
+        if(node == null) return;
+
+        System.out.print(node.getData()+" ");
+        preOrder(node.leftChild);
+        preOrder(node.rightChild);
+    }
+
+    public void inOrder(Node node) {
+        if(node == null) return;
+
+        inOrder(node.leftChild);
+        System.out.print(node.getData()+" ");
+        inOrder(node.rightChild);
+    }
+
+    public void postOrder(Node node) {
+        if(node == null) return;
+        postOrder(node.leftChild);
+        postOrder(node.rightChild);
+        System.out.print(node.getData()+" ");
+    }
+
+    static final class Node {
+        int data;
+        Node leftChild;
+        Node rightChild;
+
+        public final int getData() {
+            return data;
+        }
+
+        public final void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getLeftChild() {
+            return leftChild;
+        }
+
+        public void setLeftChild(Node leftChild) {
+            this.leftChild = leftChild;
+        }
+
+        public Node getRightChild() {
+            return rightChild;
+        }
+
+        public void setRightChild(Node rightChild) {
+            this.rightChild = rightChild;
+        }
     }
 }
